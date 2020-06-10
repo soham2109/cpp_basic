3436979b4a7a94e8e85f677f4be27da73380bd2e
Soham Naha <37201714+soham2109@users.noreply.github.com>
2020-06-10 20:56:22 +0530

    updated to retry only thrice.

--- enterPin.cpp
+++ enterPin.cpp
@@ -6,21 +6,20 @@ Codeacademy while loop demo
 
 int main() {
   
-  int pin;
-  int tries=1;
+  int pin=0;
+  int tries = 1;
   
   std::cout << "BANK OF CODECADEMY\n";
-  std::cout << "Enter your PIN: ";
-  std::cin >> pin;
-  
+
   while (pin != 1234 && tries <= 3) {
     std::cout << "Enter your PIN: ";
     std::cin >> pin;
     tries++;
   }
-  
   if (pin == 1234) {
     std::cout << "PIN accepted!\n";
     std::cout << "You now have access.\n"; 
   }
+  else
+    std::cout << "PIN not accepted!\nMaximum retries reached\n";
 }
