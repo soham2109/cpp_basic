3790979a42c3ea22c82439154c77e81e0a442636
Soham Naha <37201714+soham2109@users.noreply.github.com>
2020-06-10 18:48:41 +0530

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+  double tempf = 73.0;
+  double tempc;
+  tempc = (tempf-32)/1.8;
+  std::cout << "The temp is "<<tempc<<" degrees Celsius.\n";
+}
+/*
+working with doubles and some equations.
+*/
