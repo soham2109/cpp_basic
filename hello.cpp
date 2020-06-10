08dcb199d5d6963174d00bbdcdcacda9be82e724
Soham Naha <37201714+soham2109@users.noreply.github.com>
2020-06-10 18:08:12 +0530

    created hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// this is a comment in C++
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n" << endl;
+}
+
+// if we don't use the line "using namespace std;" then
+// we would have to use "std::cout" and "std::cin" every time.
