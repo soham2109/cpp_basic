f31e7e9702fab642a7307677bf2bf6d04a26369a
Soham Naha <37201714+soham2109@users.noreply.github.com>
2020-06-12 10:12:03 +0530

    updated full solution

--- ndigitSumOddeqSumEven.cpp
+++ ndigitSumOddeqSumEven.cpp
@@ -37,19 +37,23 @@ bool is_equal (int n)
 
 int main ()
 {
-  // input : n => n of n-digit numbers
-  int n;
-  cin >> n;
-  // the first number always starts with either 11 or 110 or 1100
-  int start = pow (10, (n - 2));
-  int val;
-
-  for (int i = 1; i <= pow (9, n - 1); i++)
+  int start, end = 0, temp, n,val;
+  cin >> n; // n as in n-digit number
+	
+  // there is a small pattern between the ending multiples of 11
+  temp = n;
+  while (temp > 1)
     {
-      if (i % 10 != 0)
-	      val = 11 * (start + i - 1);
-        if (is_equal (val))
-	        cout << val << " ";
+      end += 9 * pow (10, (temp - 2));
+      temp = temp - 2;
+    }
+  start = end / 10 + 1;
+
+  for (int i = start; i <= end; i++)
+    {
+      val = 11*i;
+      if(is_equal(val))
+	cout << 11 * i << " ";
     }
   return 0;
 }
@@ -58,10 +62,13 @@ int main ()
 So, the logic for this code goes as follows:
 - Firstly, most of the numbers that are divisible by 11 have the property
   that abs(sumOdd-sumEven)=0 or 11.
-- There are 9^(n) n-digit numbers that are divisible by 11.
-- Then the sum criterion is matched.
-
-NOTE: this code works properly for 2 and 3-digit numbers. Fails in giving output 1001 for 4-digit numbers.
-Trying to code for the generic. WILL BE UPDATING SOON!!
+- and there is a pattern that is followed by numbers divisible by 11:
+  | n |  start  | multiplying factor | end-point  | multiplying factor |
+  |---|---------|--------------------|------------|--------------------|
+  | 2 |   11    |      1             |    99      |         9          |
+  | 3 |   110   |  10 = (90//10+1)   |    990     |        90          |
+  | 4 |  1001   |  91 =(909//10 + 1) |   9999     |        909         |
+  | 5 |  10010  |  910=(9090//10 + 1)|  99990     |       9090         |
+  etc. 
 
 */
